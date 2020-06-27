41b3e0e1d88d64607b832eded848b8e5714bd232
Ernst van Graan <ernst.vangraan@gmail.com>
2020-06-27 12:27:53 -0400

    enable wsgi

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import flask
 from flask import jsonify
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
 @app.route('/api/v1/record', methods=['POST'])
 def apiRecord():
     return jsonify({'status' : 'success'})
 
-app.run()
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
