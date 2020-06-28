f4056abfcf3aee664dda4fc928b64831c2a54f8e
Ernst van Graan <ernstv@3SDS.home>
2020-06-28 07:42:43 -0400

    logging for client and POST working

--- client.py
+++ client.py
@@ -7,6 +7,11 @@ import logging
 import psutil
 
 logger = logging.getLogger("client")
+logger.setLevel(level=logging.DEBUG)
+file_handler = logging.FileHandler("client.log")
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+
 config_file = 'config.json'
 config = {}
 if os.path.exists(config_file):
@@ -38,10 +43,13 @@ data = []
 for proc in psutil.process_iter(['pid', 'username', 'name', 'status', 'cpu_num', 'num_ctx_switches', 'memory_full_info', 'connections', 'cmdline', 'create_time', 'ionice', 'num_fds', 'memory_maps', 'cpu_percent', 'terminal', 'ppid', 'cwd', 'nice', 'cpu_times', 'io_counters', 'memory_info', 'threads', 'open_files', 'num_threads', 'exe', 'uids', 'gids', 'cpu_affinity', 'memory_percent', 'environ']):
   data.append(proc.info)
 
-content = {'uuid': config['uuid'], 'timestamp': str(time.time()), 'data': json.dumps(data)}
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+content = {"uuid": config["uuid"], "timestamp": str(time.time()), "data": data}
+print(json.dumps(content))
+headers = {"Content-type": "application/json", "Accept": "text/plain"}
 try:
-  r = requests.post(url, data=content, headers=headers)
+  r = requests.post(url, data=json.dumps(content), headers=headers, verify=False)
+  logger.info("Response code: " + str(r.status_code))
+  logger.info(r.text)
 except requests.exceptions.RequestException:
     message = "POST failed"
     logger.error(message)
